b05d16b74ee43bcab0bf62b4e128098740351f41
HASHAS Jad el mawla <jouj969@gmail.com>
2023-12-30 15:12:34 +0100

    Ajout fonction multiplication

--- src/main/java/ma/ehei/calculatrice/App.java
+++ src/main/java/ma/ehei/calculatrice/App.java
@@ -5,5 +5,9 @@ public class App {
 		System.out.println(Calculatrice.add(5, 5));
 		System.out.println(Calculatrice.add(10, 10));
 		System.out.println(Calculatrice.add(100, 100));
+		
+		System.out.println(Calculatrice.multiply(5, 5));
+		System.out.println(Calculatrice.multiply(10, 10));
+		System.out.println(Calculatrice.multiply(100, 100));
 	}
 }
